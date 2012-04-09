23090a4a4fcf8b5045086dbe267a780e01681097
kerryjiang <zjiang@w-shdc-zjiang.global.tektronix.net>
2012-04-09 22:37:42 +0800

    first commit

---
+++ Protocol/DelegateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.ClientEngine.Core;
+
+namespace SuperSocket.ClientEngine.Protocol
+{
+    class DelegateCommand<TClientSession, TCommandInfo> : ICommand<TClientSession, TCommandInfo>
+        where TClientSession : IClientSession
+        where TCommandInfo : ICommandInfo
+    {
+        private Action<TClientSession, TCommandInfo> m_Execution;
+
+        public DelegateCommand(string name, Action<TClientSession, TCommandInfo> execution)
+        {
+            Name = name;
+            m_Execution = execution;
+        }
+
+        public void ExecuteCommand(TClientSession session, TCommandInfo commandInfo)
+        {
+            m_Execution(session, commandInfo);
+        }
+
+        public string Name { get; private set; }
+    }
+}
---
+++ Protocol/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Protocol")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Protocol")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5224eecf-0996-4f48-a0f2-a8690ead4d95")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
