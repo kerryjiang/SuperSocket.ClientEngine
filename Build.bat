523762bcce547e9af779326efd4f593b70d45a01
Kerry Jiang <kerryjiang@outlook.com>
2013-01-30 22:56:18 -0800

    added WP8 solution file and project file

--- Build.bat
+++ Build.bat
@@ -28,6 +28,10 @@ set msbuild=%fdir%\v4.0.30319\msbuild.exe
 
 %msbuild% SuperSocket.ClientEngine.WP71.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\WP71\Release
 
+%msbuild% SuperSocket.ClientEngine.WP80.sln /p:Configuration=Debug /t:Clean;Rebuild /p:OutputPath=..\bin\WP80\Debug
+
+%msbuild% SuperSocket.ClientEngine.WP80.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\WP80\Release
+
 set fdir=%WINDIR%\Microsoft.NET\Framework
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
 
