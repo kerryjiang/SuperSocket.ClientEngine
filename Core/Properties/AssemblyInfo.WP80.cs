5b8ec20bf0249274956052361ee85a35d847ded8
Kerry Jiang <kerryjiang@outlook.com>
2013-02-04 06:55:04 -0800

    added WP80 assemblyInfo files

---
+++ Core/Properties/AssemblyInfo.WP80.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuperSocket.ClientEngine.Core for Windows Phone 8.0")]
+[assembly: AssemblyDescription("SuperSocket.ClientEngine.Core")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("33F89486-B096-4EE6-8846-95237A39A37D")]
---
+++ Protocol/Properties/AssemblyInfo.WP80.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuperSocket.ClientEngine.Protocol for Windows Phone 8.0")]
+[assembly: AssemblyDescription("SuperSocket.ClientEngine.Protocol")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("32989C43-E19F-4DB7-9999-A21C30C5F09D")]
