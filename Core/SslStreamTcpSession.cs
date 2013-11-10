48bf349c318a03368847298e931196039b45a3f4
Kerry Jiang <kerry-jiang@hotmail.com>
2013-11-11 00:08:13 +0800

    fired the socket closed event after the tls handshake fail

--- Core/SslStreamTcpSession.cs
+++ Core/SslStreamTcpSession.cs
@@ -69,6 +69,7 @@ namespace SuperSocket.ClientEngine
             }
             catch(Exception e)
             {
+                EnsureSocketClosed();
                 OnError(e);
                 return;
             }
