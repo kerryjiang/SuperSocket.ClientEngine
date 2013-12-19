ffdd5c6ce7353ef41df00a313908aca5ee16d498
Kerry Jiang <kerry-jiang@hotmail.com>
2013-12-19 11:56:21 +0800

    added support for .NET 4.5

---
+++ Core/Properties/AssemblyInfo.Net45.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SuperSocket.ClientEngine.Core for .NET 4.5")]
+[assembly: AssemblyDescription("SuperSocket.ClientEngine.Core")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fbbb2183-5bc3-4bbe-8f09-eb2d55d97d72")]
---
+++ Protocol/Properties/AssemblyInfo.Net45.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SuperSocket.ClientEngine.Protocol for .NET 4.5")]
+[assembly: AssemblyDescription("SuperSocket.ClientEngine.Protocol")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("AAAD620D-6E44-40A5-9D50-6FB73EF0A4DC")]
