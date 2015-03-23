c16fa997eea837af576cd5ecb4acc13475700d87
Kerry Jiang <kerry-jiang@hotmail.com>
2015-03-23 09:32:05 +0800

    Support iOS Unified API

---
+++ Proxy/Properties/AssemblyInfo.iOS.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SuperSocket.ClientEngine.Proxy for iOS Unified API")]
+[assembly: AssemblyDescription("SuperSocket.ClientEngine.Proxy")]
+[assembly: ComVisible(false)]
+[assembly: Guid("84D7E6D8-350F-4FB7-8FF4-4D869A0537AF")]
