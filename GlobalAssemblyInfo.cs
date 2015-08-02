a6dd12ade5241cf8c3df1ad9530cf15703c60bf0
Kerry Jiang <kerry-jiang@hotmail.com>
2015-08-03 00:23:14 +0800

    updated version and copy right in the global assembly information

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
 [assembly: AssemblyFileVersion("0.3.0.0")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket ClientEngine")]
-[assembly: AssemblyInformationalVersion("0.3.0.0")]
-[assembly: AssemblyCopyright("Copyright © clientengine.codeplex.com 2012")]
+[assembly: AssemblyInformationalVersion("0.4.0.0")]
+[assembly: AssemblyCopyright("Copyright © clientengine.codeplex.com 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
