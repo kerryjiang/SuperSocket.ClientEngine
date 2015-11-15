33c7c6f4fb909694540bf1c42e7925474a3a6117
Kerry Jiang <kerry-jiang@hotmail.com>
2015-11-15 13:54:05 +0800

    improved the build script

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.6.0.2")]
+[assembly: AssemblyFileVersion("0.6.0.2")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket ClientEngine")]
-[assembly: AssemblyInformationalVersion("0.6.0.0")]
+[assembly: AssemblyInformationalVersion("0.6.0.2")]
 [assembly: AssemblyCopyright("Copyright © clientengine.codeplex.com 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
