aeb4395f1230a85b8c1ff68efc6790c360a053ba
Kerry Jiang <kerry-jiang@hotmail.com>
2016-05-31 15:27:26 +0800

    re-organized the projects

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.7.0.1")]
-[assembly: AssemblyFileVersion("0.7.0.1")]
+[assembly: AssemblyVersion("0.7.0.2")]
+[assembly: AssemblyFileVersion("0.7.0.2")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket ClientEngine")]
-[assembly: AssemblyInformationalVersion("0.7.0.1")]
+[assembly: AssemblyInformationalVersion("0.7.0.2")]
 [assembly: AssemblyCopyright("Copyright © clientengine.codeplex.com 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
