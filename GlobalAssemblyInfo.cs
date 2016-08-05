88a95213392e26638d6060ce0ea8a316822b52e3
Kerry Jiang <kerry-jiang@hotmail.com>
2016-08-05 21:31:39 +0800

    upgraded ProtoBase to 1.7.1

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.7.0.2")]
-[assembly: AssemblyFileVersion("0.7.0.2")]
+[assembly: AssemblyVersion("0.7.1.0")]
+[assembly: AssemblyFileVersion("0.7.1.0")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket ClientEngine")]
-[assembly: AssemblyInformationalVersion("0.7.0.2")]
+[assembly: AssemblyInformationalVersion("0.7.1.0")]
 [assembly: AssemblyCopyright("Copyright © clientengine.codeplex.com 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
