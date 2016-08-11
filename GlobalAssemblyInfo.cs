5910100fbf43a3ffafc01a463673c70ef08556eb
Kerry Jiang <kerry-jiang@hotmail.com>
2016-08-11 16:27:54 +0800

    updated SuperSocket.ProtoBase to 1.7.0.11

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.8.0.2")]
-[assembly: AssemblyFileVersion("0.8.0.2")]
+[assembly: AssemblyVersion("0.8.0.3")]
+[assembly: AssemblyFileVersion("0.8.0.3")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket ClientEngine")]
-[assembly: AssemblyInformationalVersion("0.8.0.2")]
+[assembly: AssemblyInformationalVersion("0.8.0.3")]
 [assembly: AssemblyCopyright("Copyright © clientengine.codeplex.com 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
