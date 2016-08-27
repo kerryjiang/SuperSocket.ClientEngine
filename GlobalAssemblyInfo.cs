71701aabefdd9e71fb1130410da475d4b427a3aa
Kerry Jiang <kerry-jiang@hotmail.com>
2016-08-27 16:53:32 +0800

    upgraded SuperSocket.ClientEngine to 1.7.0.12

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.8.0.6")]
-[assembly: AssemblyFileVersion("0.8.0.6")]
+[assembly: AssemblyVersion("0.8.0.7")]
+[assembly: AssemblyFileVersion("0.8.0.7")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket ClientEngine")]
-[assembly: AssemblyInformationalVersion("0.8.0.6")]
+[assembly: AssemblyInformationalVersion("0.8.0.7")]
 [assembly: AssemblyCopyright("Copyright © clientengine.codeplex.com 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
