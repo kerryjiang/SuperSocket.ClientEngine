c4b2e901a70aa9d302646d46dbdacce22ebad968
Kerry Jiang <kerry-jiang@hotmail.com>
2017-10-17 22:47:39 -0700

    upgraded version number

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.8.0.13")]
-[assembly: AssemblyFileVersion("0.8.0.13")]
+[assembly: AssemblyVersion("0.8.0.14")]
+[assembly: AssemblyFileVersion("0.8.0.14")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket ClientEngine")]
-[assembly: AssemblyInformationalVersion("0.8.0.13")]
+[assembly: AssemblyInformationalVersion("0.8.0.14")]
 [assembly: AssemblyCopyright("Copyright © www.supersocket.net 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
